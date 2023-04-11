f1bacaa021d036896e7b4baae8d8fa17e80fc3b3
Jake Hebert <hebertj@etsu.edu>
2023-04-11 11:16:32 -0400

    pain

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 console.log("Hello World!");
 var counter = 20;
 while (counter < 30){
-    console.log(`Counter: ${counter}`);
     counter++
+    console.log(`Counter: ${counter}`);
     $("#counter").html(counter);
 }
