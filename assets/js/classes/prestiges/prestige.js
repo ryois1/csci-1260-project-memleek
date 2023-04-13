594c9911bce2c44c716d42d1a54b3d84c645c28f
Russell Payne <ryois@ryois.me>
2023-04-13 15:43:21 +0000

    Trying to add break infinity
    Co-authored-by: Brendan Dalhover <SparkSages@users.noreply.github.com>
    Co-authored-by: Russell Payne <ryois@ryois.me>
    Co-authored-by: Jakehebert201 <Jakehebert201@users.noreply.github.com>
    Co-authored-by: Dep16 <depDesai16@users.noreply.github.com>

--- assets/js/classes/prestiges/prestige.js
+++ assets/js/classes/prestiges/prestige.js
@@ -1,45 +1,55 @@
 'use strict';
-class Prestige{
+import { Miner8 } from '../m/m8.js';
+class Prestige {
 
     id;
     // id of the compressors (similar to infinity dimensions)
     upgradeID;
     // id of the prestige upgrades that will impact the multiplier
-    production;
+    production = new Decimal(1);
     // production of the compressors
-    compressQuantity;
+    compressQuantity = new Decimal(0);
     // quantity of the compressors
-    cost;
+    cost = new Decimal(1);
     // cost of the compressors
-    multiplier = 1;
+    multiplier = new Decimal(1);
     // default multiplier property of a prestige
-    compressionPointsEarned = 0;
+    compressionPointsEarned = new Decimal(0);
     // amount of points a player has earned in this prestige layer
-    globalCompressionPoints = 0;
+    globalCompressionPoints = new Decimal (0);
     // amount of points a player has available to spend
     broken;
 
-    constructor(id, production, _compressQuantity, cost){
+    constructor(id, production, _compressQuantity, cost) {
         this.id = id;
-        this.production = production;
-        this.compressQuantity = _compressQuantity;
-        this.cost = cost;
+        this.production = new Decimal(production);
+        this.compressQuantity = new Decimal(_compressQuantity);
+        this.cost = new Decimal(cost);
     }
-    Compress(){
-        if(globalBytes >= 1.0e+49 || this.broken == true){
+    Compress() {
+        if (globalBytes >= 1.0e+49 || this.broken == true) {
             this.globalCompressionPoints += this.compressionPointsEarned;
             this.compressionPointsEarned = 0;
             return this.globalCompressionPoints;
         }
-        else{
+        else {
             return;
             //  user doesnt meet the requirements to compress
         }
     }
     //this function is called when a player clicks the compress button
+    CompressReset() {
+        this.Compress();
+        minerInstances[7].sacrificemult = 1;
+        minerInstances[7].lastsacrificequantity = 0;
+        minerInstances.forEach(function (m) {
+            m.minerQuantity = 0;
+
+        });
+    }
 
 
-    EarnCompressionPoints(){
+    EarnCompressionPoints() {
         this.compressionPointsEarned += Math.ceil(Math.log10(Math.log10(globalBytes)));
         //this is a ceiling function of the log base 100 of total bytes produced, being the amount of compression points earned in the compression
 
@@ -52,3 +62,5 @@ class Prestige{
         the current prestige layer
     */
 }
+
+export { Prestige };
